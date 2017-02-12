c1139916a02e8b060505b61aeadf304b5ca29cde
Mark Easterly <meeaster@oakland.edu>
2017-02-12 15:08:46 -0500

    initial solution

---
+++ Client/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Route } from '@angular/router';
+
+// Container (aka: "pages") imports
+import {
+    HomeComponent,
+    RestTestComponent,
+    BootstrapComponent,
+    LoginComponent,
+    ExamplesComponent,
+    ChatComponent,
+    NotFoundComponent
+} from 'app-containers';
+
